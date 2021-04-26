464ee156ddb2b5c92ef36d9320f316de9f9f50c2
zavyalovdv <daniilzavialov@yandex.ru>
2021-04-26 12:13:00 +0300

    Test tests

--- hospital/settings.py
+++ hospital/settings.py
@@ -108,10 +108,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
